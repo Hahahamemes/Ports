9249804f4a556f60f2848ec3a6d58a8f342b4687
Pepe <Pepe@DESKTOP-VMPM3FH>
2016-10-21 15:43:06 +1300

    Sharpshooter Reworked

---
+++ SharpShooter/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using LeagueSharp.Common;
+
+using EloBuddy; 
+ using LeagueSharp.Common; 
+ namespace SharpShooter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            EloBuddy.SDK.Events.Loading.OnLoadingComplete += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Initializer.Initialize();
+        }
+    }
+}
