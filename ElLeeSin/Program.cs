25e660c011f079a0a201fd99d467bc3c8253e22d
Pepe <Pepe@DESKTOP-VMPM3FH>
2016-10-22 04:47:10 +1300

    .

--- ElLeeSin/Program.cs
+++ ElLeeSin/Program.cs
@@ -574,7 +574,7 @@ using EloBuddy;
             }
 
             var prediction = spells[Spells.Q].GetPrediction(target);
-            if (prediction.Hitchance >= HitChance.VeryHigh && prediction.CastPosition.Distance(ObjectManager.Player.ServerPosition) < spells[Spells.Q].Range)
+            if (prediction.Hitchance >= HitChance.Low && prediction.CastPosition.Distance(ObjectManager.Player.ServerPosition) < spells[Spells.Q].Range)
             {
                 spells[Spells.Q].Cast(target);
             }
