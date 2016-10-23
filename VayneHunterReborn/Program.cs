91fe274456aeef7253d250d1b41e4e8d1c5902e9
Pepe <Pepe@DESKTOP-VMPM3FH>
2016-10-23 18:25:56 +1300

    VHR

---
+++ VayneHunterReborn/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+using VayneHunter_Reborn.Utility;
+
+using EloBuddy; 
+ using LeagueSharp.Common; 
+ namespace VayneHunter_Reborn
+{
+    class Program
+    {
+        private static string ChampionName = "Vayne";
+        static void Main(string[] args)
+        {
+            EloBuddy.SDK.Events.Loading.OnLoadingComplete += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.ChampionName != ChampionName)
+            {
+                return;
+            }
+            VHRBootstrap.OnLoad();
+
+            Chat.Print("<font color='#FF0000'><b>[VHR - Rewrite!]</b></font> By Asuna Loaded!");
+            Chat.Print("Also try <font color='#66FF33'><b>DZAwareness</b></font> for a gamebreaking experience!");
+        }
+    }
+}
