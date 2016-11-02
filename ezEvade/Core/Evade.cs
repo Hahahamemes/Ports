7d478f9cc8ede59a829aea491382b771ebf89e44
Pepe <Pepe@DESKTOP-VMPM3FH>
2016-11-03 01:53:52 +1300

    Loadup

--- ezEvade/Core/Evade.cs
+++ ezEvade/Core/Evade.cs
@@ -80,7 +80,7 @@ using EloBuddy;
                 }
                 else
                 {
-                    Game.OnLoad += Game_OnGameLoad;
+                    EloBuddy.SDK.Events.Loading.OnLoadingComplete += Game_OnGameLoad;
                 }
             });
         }
